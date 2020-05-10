cd6954fdb8eb563a45c26ce1a2c6d41689c4efae
amrits-git <58998659+amrits-git@users.noreply.github.com>
2020-05-10 19:38:50 -0400

    tryin to get js events and server side to work

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import { checkForName } from './client/js/nameChecker'
 import { handleSubmit } from './client/js/formHandler'
-import './client/styles/resets.scss'
 
-console.log(checkForName);
-alert("I exist")
+import './client/styles/resets.scss'
+import './client/styles/base.scss'
+import './client/styles/footer.scss'
+import './client/styles/form.scss'
+import './client/styles/header.scss'
+
+export {
+    checkForName,
+    handleSubmit
+}
